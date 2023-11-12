70f571e3df49490462fe886f91e357aeaa1d5737
RMCDS <remersonmcds@gmail.com>
2023-11-12 17:00:29 -0300

    Backup

--- app/src/main/java/com/example/healthremiderrebuild/areaDcadastro.java
+++ app/src/main/java/com/example/healthremiderrebuild/areaDcadastro.java
@@ -2,7 +2,9 @@ package com.example.healthremiderrebuild;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class areaDcadastro extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class areaDcadastro extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.area_cadastro);
+    }
+
+    public void voltarTareaCadastro(View view) {
+        Intent intent = new Intent(this, SegundaTela.class);
+        startActivity(intent);
     }
 }
